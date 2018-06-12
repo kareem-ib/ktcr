66a856800ca0bb7289cd39026d8bcc6dd8b91dc4
Isaac Kang <isaac.kang@consensys.net>
2018-06-12 07:46:59 -0400

    update readme & more...
    many users have been experiencing difficulty when trying to get
    up-and-running using the tcr & tcr-ui repos together. this commit
    makes bootstrapping the two systems much easier.
    
    * bump truffle deps
    * check for MNEMONIC environment variable
    * exit if: (mainnet && default_mnemonic)
    * add ganache network
    * add default secrets.json
    * add warnings

--- truffle.js
+++ truffle.js
@@ -1,7 +1,25 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const fs = require('fs');
 
-const mnemonic = process.env.MNEMONIC || '';
+let mnemonic = '';
+
+if (fs.existsSync('secrets.json')) {
+  const secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+  ({ mnemonic } = secrets);
+}
+
+if (process.env.MNEMONIC) {
+  mnemonic = process.env.MNEMONIC;
+}
+
+if (mnemonic.includes('candy maple cake') && process.argv[4] !== 'mainnet') {
+  console.log(
+    'WARNING! It looks like you are trying to deploy to the Ethereum Main Network using a mnemonic',
+    'that looks suscpiciously like the default ganache mnemonic. ',
+    'See more here: https://github.com/trufflesuite/ganache/issues/275. Exiting...',
+  );
+  process.exit(1);
+}
 
 module.exports = {
   networks: {
