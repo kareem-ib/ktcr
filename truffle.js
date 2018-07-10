6f24db8eb1729c5025139a98337f689194fe6c2a
Isaac Kang <isaac.kang@consensys.net>
2018-07-10 11:59:49 -0400

    lambda HDProvider
    See: trufflesuite/truffle#1022

--- truffle.js
+++ truffle.js
@@ -24,25 +24,25 @@ if (mnemonic.includes('candy maple cake') && process.argv[4] === 'mainnet') {
 module.exports = {
   networks: {
     mainnet: {
-      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
+      provider: () => new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
       network_id: '1',
       gas: 4500000,
       gasPrice: 10000000000,
     },
     ganache: {
-      provider: new HDWalletProvider(mnemonic, 'http://localhost:8545'),
+      provider: () => new HDWalletProvider(mnemonic, 'http://localhost:8545'),
       network_id: '*',
       gas: 4500000,
       gasPrice: 25000000000,
     },
     rinkeby: {
-      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
       network_id: '*',
       gas: 4500000,
       gasPrice: 25000000000,
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
+      provider: () => new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
       network_id: '*',
       gas: 4500000,
       gasPrice: 25000000000,
